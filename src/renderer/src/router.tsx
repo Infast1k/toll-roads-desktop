8fb0e825fdb6fc7c73582fec74e9c2aaf31a689d
Mihail <infast1k@mail.ru>
2024-09-05 23:47:47 +0300

    feat: implement login page without connecting to api

--- src/renderer/src/router.tsx
+++ src/renderer/src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import LoginPage from '@renderer/pages/LoginPage'
+import RegisterPage from '@renderer/pages/RegisterPage'
 
 const router = createBrowserRouter([
   {
     path: '/login',
     element: <LoginPage />
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />
   }
 ])
 
