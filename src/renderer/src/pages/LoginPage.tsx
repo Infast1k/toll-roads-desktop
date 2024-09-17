bbd4e83ba20b0881a8887f45e36971dc0a7fc2a0
Mihail <infast1k@mail.ru>
2024-09-17 16:59:54 +0300

    feat: create connection to API for login (without exception handling)

--- src/renderer/src/pages/LoginPage.tsx
+++ src/renderer/src/pages/LoginPage.tsx
@@ -1,6 +1,41 @@
 import Logo from '@renderer/components/logo'
+import axios from 'axios'
+import { FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function LoginPage(): JSX.Element {
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [formIsValid, setFormIsValid] = useState(true)
+
+  const handleFormError = () => {
+    setFormIsValid(false)
+    setPassword('')
+  }
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    axios
+      .post('http://localhost:8000/api/v1/sign-in/', {
+        email: email,
+        password: password
+      })
+      .then((response) => {
+        localStorage.setItem('access_token', response.data.access_token)
+        localStorage.setItem('refresh_token', response.data.refresh_token)
+
+        navigate('/', { replace: true })
+      })
+      .catch((err) => {
+        // TODO: handle errors
+        console.error(err)
+        handleFormError()
+      })
+  }
+
   return (
     <>
       <div className="flex min-h-full flex-col justify-center text-text-color">
@@ -8,15 +43,17 @@ function LoginPage(): JSX.Element {
           <div className="py-6 sm:mx-auto sm:w-full sm:max-w-sm">
             <Logo height={100} width={100} />
             <h1 className="mb-7 text-center text-3xl font-medium">Sign in to account</h1>
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={onSubmit}>
               <div>
                 <label className="input-label" htmlFor="labeled-input">
                   Email
                 </label>
                 <input
-                  className="block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] border-border-color focus:border-primary-color outline-none"
+                  className={`block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] ${formIsValid ? 'border-border-color' : 'border-border-exception-color'} focus:border-primary-color outline-none`}
                   type="email"
                   id="labeled-input"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                   required
                 />
               </div>
@@ -26,9 +63,11 @@ function LoginPage(): JSX.Element {
                     Password
                   </label>
                   <input
-                    className="block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] border-border-color focus:border-primary-color outline-none"
+                    className={`block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] ${formIsValid ? 'border-border-color' : 'border-border-exception-color'} focus:border-primary-color outline-none`}
                     type="password"
                     id="labeled-input"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
                     required
                   />
                 </div>
@@ -45,7 +84,7 @@ function LoginPage(): JSX.Element {
                 </button>
               </div>
             </form>
-            <div className="my-2 leading-6 font-medium text-sm">
+            <div className="my-3 leading-6 font-medium text-sm">
               <p className="text-center text-muted">
                 Don&apos;t have an account?&nbsp;
                 <a
