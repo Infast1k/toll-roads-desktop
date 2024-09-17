050624d5051162f89bed871814028a543769627f
Mihail <infast1k@mail.ru>
2024-09-17 16:21:58 +0300

    feat: create connection to API for registration (without exception handling)

--- src/renderer/src/pages/RegisterPage.tsx
+++ src/renderer/src/pages/RegisterPage.tsx
@@ -1,6 +1,51 @@
 import Logo from '@renderer/components/logo'
+import axios from 'axios'
+import { FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 
-function LoginPage(): JSX.Element {
+function RegisterPage(): JSX.Element {
+  const navigate = useNavigate()
+
+  const [companyName, setCompanyName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [password1, setPassword1] = useState('')
+  const [formIsValid, setFormIsValid] = useState(true)
+
+  const handleFormError = () => {
+    setFormIsValid(false)
+    setPassword('')
+    setPassword1('')
+  }
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if (password !== password1) {
+      handleFormError()
+      // TODO: handle errors
+      return
+    }
+
+    axios
+      .post('http://localhost:8000/api/v1/sign-up/', {
+        name: companyName.toLowerCase(),
+        account: {
+          email: email.toLowerCase(),
+          password: password.toLowerCase()
+        }
+      })
+      .then(() => {
+        navigate('/login', { replace: true })
+      })
+      .catch((err) => {
+        // TODO: handle errors
+        console.error(err)
+        handleFormError()
+      })
+  }
+
   return (
     <>
       <div className="flex min-h-full flex-col justify-center text-text-color">
@@ -8,15 +53,30 @@ function LoginPage(): JSX.Element {
           <div className="py-6 sm:mx-auto sm:w-full sm:max-w-sm">
             <Logo height={100} width={100} />
             <h1 className="mb-7 text-center text-3xl font-medium">Sign up an account</h1>
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={onSubmit}>
+              <div>
+                <label className="input-label" htmlFor="labeled-input">
+                  Company name
+                </label>
+                <input
+                  className={`block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] ${formIsValid ? 'border-border-color' : 'border-border-exception-color'} focus:border-primary-color outline-none`}
+                  type="text"
+                  id="labeled-input"
+                  onChange={(e) => setCompanyName(e.target.value)}
+                  value={companyName}
+                  required
+                />
+              </div>
               <div>
                 <label className="input-label" htmlFor="labeled-input">
                   Email
                 </label>
                 <input
-                  className="block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] border-border-color focus:border-primary-color outline-none"
+                  className={`block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] ${formIsValid ? 'border-border-color' : 'border-border-exception-color'} focus:border-primary-color outline-none`}
                   type="email"
                   id="labeled-input"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                   required
                 />
               </div>
@@ -26,9 +86,11 @@ function LoginPage(): JSX.Element {
                     Password
                   </label>
                   <input
-                    className="block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] border-border-color focus:border-primary-color outline-none"
+                    className={`block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] ${formIsValid ? 'border-border-color' : 'border-border-exception-color'} focus:border-primary-color outline-none`}
                     type="password"
                     id="labeled-input"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
                     required
                   />
                 </div>
@@ -39,15 +101,20 @@ function LoginPage(): JSX.Element {
                     Repeat password
                   </label>
                   <input
-                    className="block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] border-border-color focus:border-primary-color outline-none"
+                    className={`block input w-full rounded-sm h-8 pl-2 bg-foreground-color border-[0.5px] ${formIsValid ? 'border-border-color' : 'border-border-exception-color'} focus:border-primary-color outline-none`}
                     type="password"
                     id="labeled-input"
+                    onChange={(e) => setPassword1(e.target.value)}
+                    value={password1}
                     required
                   />
                 </div>
               </div>
               <div className="pt-6">
-                <button className="button text-base md:text-sm bg-primary-color hover:bg-primary-hover-color w-full h-8 rounded-sm transition ease-linear">
+                <button
+                  className="button text-base md:text-sm bg-primary-color hover:bg-primary-hover-color w-full h-8 rounded-sm transition ease-linear"
+                  type="submit"
+                >
                   Sign up
                 </button>
               </div>
@@ -70,4 +137,4 @@ function LoginPage(): JSX.Element {
   )
 }
 
-export default LoginPage
+export default RegisterPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/renderer/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'background-color': 'var(--background-color)',
-        'foreground-color': 'var(--foreground-color)',
-        'border-color': 'var(--border-color)',
-        'text-color': 'var(--text-color)',
-        'primary-color': 'var(--primary-color)',
-        'primary-hover-color': 'var(--primary-hover-color)'
-      }
+export const content = ['./src/renderer/**/*.{js,ts,jsx,tsx}']
+export const theme = {
+  extend: {
+    colors: {
+      'background-color': 'var(--background-color)',
+      'foreground-color': 'var(--foreground-color)',
+      'border-color': 'var(--border-color)',
+      'border-exception-color': 'var(--border-exception-color)',
+      'text-color': 'var(--text-color)',
+      'primary-color': 'var(--primary-color)',
+      'primary-hover-color': 'var(--primary-hover-color)'
     }
-  },
-  plugins: []
+  }
 }
+export const plugins = []
