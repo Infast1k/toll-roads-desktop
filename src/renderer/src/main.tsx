0d0aba9ff1bcf7efd1b3533a8248fb6f186e961a
Mihail <infast1k@mail.ru>
2024-09-21 18:38:29 +0300

    feat(ui): configure react-toastify

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,7 +3,12 @@ import '@renderer/assets/index.css'
 
 import ReactDOM from 'react-dom/client'
 import router from '@renderer/router'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
+  <>
+    <RouterProvider router={router} />
+    <ToastContainer />
+  </>
 )
