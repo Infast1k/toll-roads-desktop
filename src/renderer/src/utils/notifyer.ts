4ea9e55d2e31539c1d759d0ae5f17d9d17299e47
Mihail <infast1k@mail.ru>
2024-10-09 23:22:03 +0300

    fix(types): change returns type for notify method

--- src/renderer/src/utils/notifyer.ts
+++ src/renderer/src/utils/notifyer.ts
@@ -1,7 +1,7 @@
-import { toast } from "react-toastify"
+import { toast } from 'react-toastify'
 
 const notify = (message: string, isError: boolean) => {
-    isError ? toast.error(message) : toast.success(message)
+  isError ? toast.error(message) : toast.success(message)
 }
 
 export default notify
