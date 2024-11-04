0e97ef3b16399141590d3b18d4ec691ca25f6959
Mihail <infast1k@mail.ru>
2024-11-04 11:56:30 +0300

    feat(ui): add animation and logic for show some components

--- src/renderer/src/components/pagination.tsx
+++ src/renderer/src/components/pagination.tsx
@@ -1,78 +1,103 @@
+import { useState } from "react"
+
 export interface PaginationProps {
     inOnePage: number
     total: number
 }
 
 const Pagination: React.FC<PaginationProps> = (props: PaginationProps) => {
-    const activeStyle = "text-white font-semibold border border-primary-color border-solid bg-primary-color"
+    const defaultStyle = "flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem] hover:bg-primary-hover-color"
+    const activeStyle = defaultStyle + "text-white font-semibold border border-primary-color border-solid bg-primary-color rounded-[0.4rem] hover:bg-primary-hover-color"
+
+    const pageCount = Math.ceil(props.total / props.inOnePage)
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const pages = Array.from({ length: pageCount }, (_, index) => {
+        const page = index + 1
+        return (
+            <li
+                key={page}
+                className={page === currentPage ? activeStyle : defaultStyle}
+                onClick={() => setCurrentPage(page)}
+            >
+                <span>{page}</span>
+            </li>
+        )
+    })
 
     return (
         <>
             <div className="justify-center items-center text-text-color mt-8">
-                <ul className="list-none flex justify-center items-center h-10 mx-auto max-w-[600px] shadow-[0_0.8rem_2rem_rgba(#5a6181,0.05)] rounded-[0.5rem] bg-foreground-color">
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem]">
-                        <span className="material-icons">
-                            <svg 
-                                width="24" 
-                                height="24" 
-                                viewBox="0 0 24 24" 
-                                fill="none" 
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path 
-                                    d="M19.5 12H6" 
-                                    stroke="#7287fd" 
-                                    stroke-width="2" 
-                                    stroke-linecap="round" 
-                                    stroke-linejoin="round"
-                                />
-                                <path 
-                                    d="M11 17.5L5 12L11 6.5" 
-                                    stroke="#7287fd"
-                                    stroke-width="2" 
-                                    stroke-linecap="round" 
-                                    stroke-linejoin="round"
-                                />
-                            </svg>
-                        </span>
-                    </li>   
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem]"> 1</li>
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem]">2</li>
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem]">3</li>
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem]">4</li>
-                    <li className={`flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem] ${activeStyle}`}>5</li>
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem]">6</li>
-                    <li className="flex flex-col items-center justify-center m-[0.3rem] w-[2.6rem] h-[2.6rem] cursor-[initial]">...</li>
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem] rounded-[0.4rem]"> 10</li>
-                    <li className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem]">
-                        <span className="material-icons">
-                            <svg 
-                                width="24" 
-                                height="24" 
-                                viewBox="0 0 24 24"
-                                fill="none" 
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path 
-                                    d="M18.5 12H4" 
-                                    stroke="#7287fd" 
-                                    stroke-width="2"
-                                    stroke-linecap="round" 
-                                    stroke-linejoin="round"
-                                />
-                                <path 
-                                    d="M13 6.5L18.5 12L13 17.5" 
-                                    stroke="#7287fd" 
-                                    stroke-width="2" 
-                                    stroke-linecap="round" 
-                                    stroke-linejoin="round"
-                                />
-                            </svg>
-                        </span>
-                    </li>
+                <ul className="list-none flex justify-center items-center h-10 mx-auto max-w-[500px] shadow-[0_0.8rem_2rem_rgba(#5a6181,0.05)] rounded-[0.5rem] bg-foreground-color">
+                    {pageCount > 1 ? (
+                        <li
+                            className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem]"
+                            onClick={() => currentPage > 1 ? setCurrentPage(currentPage - 1) : setCurrentPage(pageCount)}
+                        >
+                            <span className="material-icons">
+                                <svg 
+                                    width="24" 
+                                    height="24" 
+                                    viewBox="0 0 24 24" 
+                                    fill="none" 
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path 
+                                        d="M19.5 12H6" 
+                                        stroke="#7287fd" 
+                                        stroke-width="2" 
+                                        stroke-linecap="round" 
+                                        stroke-linejoin="round"
+                                    />
+                                    <path 
+                                        d="M11 17.5L5 12L11 6.5" 
+                                        stroke="#7287fd"
+                                        stroke-width="2" 
+                                        stroke-linecap="round" 
+                                        stroke-linejoin="round"
+                                    />
+                                </svg>
+                            </span>
+                        </li>
+                    ) : (
+                        <></>
+                    )}
+                    {pages}
+                    {pageCount > 1 ? (
+                        <li
+                            className="flex flex-col items-center justify-center cursor-pointer m-[0.3rem] w-[2.6rem] h-[2.6rem]"
+                            onClick={() => currentPage < pageCount ? setCurrentPage(currentPage + 1) : setCurrentPage(1)}
+                        >
+                            <span className="material-icons">
+                                <svg 
+                                    width="24" 
+                                    height="24" 
+                                    viewBox="0 0 24 24"
+                                    fill="none" 
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path 
+                                        d="M18.5 12H4" 
+                                        stroke="#7287fd" 
+                                        stroke-width="2"
+                                        stroke-linecap="round" 
+                                        stroke-linejoin="round"
+                                    />
+                                    <path 
+                                        d="M13 6.5L18.5 12L13 17.5" 
+                                        stroke="#7287fd" 
+                                        stroke-width="2" 
+                                        stroke-linecap="round" 
+                                        stroke-linejoin="round"
+                                    />
+                                </svg>
+                            </span>
+                        </li>
+                    ) : (
+                        <></>
+                    )}
                 </ul>
             </div>
         </>
     )
 }
-
 
 export default Pagination
